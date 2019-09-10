a55a1503d39ea01e714a22d9837245798832c3e0
Rodrigo Kamada <rodrigo@kamada.com.br>
2019-09-10 19:39:21 -0300

    Adding the typedoc.

--- src/lib/contents/template.ts
+++ src/lib/contents/template.ts
@@ -1,11 +1,20 @@
 import { AbstractContent } from './abstract-content';
 import { ITemplateContent } from '../../types';
 
+/**
+ * Implementation of template content.
+ */
 export class TemplateContent extends AbstractContent implements ITemplateContent {
 
   templateId: string;
   fields: { [name: string]: string; };
 
+  /**
+   * Returns a new `TemplateContent` that can be used to send message templates to your customer.
+   *
+   * @param templateId The template identifier on the Zenvia platform.
+   * @param fields The array of values to apply in the template.
+   */
   constructor(id: string, fields: { [name: string]: string; }) {
     super('template');
     this.templateId = id;
--- src/lib/subscriptions/message-status.ts
+++ src/lib/subscriptions/message-status.ts
@@ -1,8 +1,18 @@
 import { IWebhook, ICriteria, SubscriptionStatus } from '../../types';
 import { Subscription } from './base';
 
+/**
+ * Implementation of message status subscription.
+ */
 export class MessageStatusSubscription extends Subscription {
 
+  /**
+   * Returns a new `MessageStatusSubscription` that is used to create a new message status subscription.
+   *
+   * @param webhook The [[IWebhook]] object.
+   * @param criteria The [[ICriteria]] object.
+   * @param status Status of subscription. The [[SubscriptionStatus]] object.
+   */
   constructor(webhook: IWebhook, criteria: ICriteria, status: SubscriptionStatus = 'ACTIVE') {
     super('MESSAGE_STATUS', webhook, criteria, status);
   }
