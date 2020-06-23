f3231d889acb789ebc65c9700fe897528b2019bb
Henrique Magarotto <magarotto@gmail.com>
2020-06-23 16:51:59 -0300

    Removed payload from TextContent

--- src/lib/contents/text.ts
+++ src/lib/contents/text.ts
@@ -7,17 +7,15 @@ import { ITextContent } from '../../types';
 export class TextContent extends AbstractContent implements ITextContent {
 
   text: string;
-  payload: string;
 
   /**
    * Returns a new `TextContent` that can be used to send text messages to your customer.
    *
    * @param text The text of the message.
    */
-  constructor(text: string, payload?: string) {
+  constructor(text: string) {
     super('text');
     this.text = text;
-    this.payload = payload;
   }
 
 }
