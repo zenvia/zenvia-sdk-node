805ec26325b84ea2dba617e9c010ec1dadabeb3c
isabela.nascimento <isabela.nascimento@zenvia.com>
2022-06-08 16:43:53 -0300

    fix: finxing identation

--- src/lib/contents/card.ts
+++ src/lib/contents/card.ts
@@ -1,31 +1,31 @@
-import { Buttons, ICardContent, IMedia } from "../../types";
-import { AbstractContent } from "./abstract-content";
+import { Buttons, ICardContent, IMedia } from '../../types';
+import { AbstractContent } from './abstract-content';
 
 /**
  * Implementation of card content.
  */
 export class CardContent extends AbstractContent implements ICardContent {
-    title?: string;
-    text?: string;
-    media?: IMedia;
-    buttons?: Buttons;
-    quickReplyButtons?: Buttons;
-    
-    /**
-     * Returns a new `CardContent` that can be used to send an image, a GIF or a video to your customer.
-     * 
-     * @param title A title in bold shown above the text of the card.
-     * @param text A text to be displayed inside the card.
-     * @param media  Object with the media url, disposition and caption.
-     * @param buttons List of buttons displayed inside a card.
-     * @param quickReplyButtons Horizontal list of buttons displayed after the content.
-     */
-    constructor(title?: string, text?: string, media?: IMedia, buttons?: Buttons, quickReplyButtons?: Buttons) {
-        super('card');
-        this.title = title;
-        this.text = text;
-        this.media = media;
-        this.buttons = buttons;
-        this.quickReplyButtons = quickReplyButtons;
-    }
+  title?: string;
+  text?: string;
+  media?: IMedia;
+  buttons?: Buttons;
+  quickReplyButtons?: Buttons;
+
+  /**
+   * Returns a new `CardContent` that can be used to send an image, a GIF or a video to your customer.
+   *
+   * @param title A title in bold shown above the text of the card.
+   * @param text A text to be displayed inside the card.
+   * @param media  Object with the media url, disposition and caption.
+   * @param buttons List of buttons displayed inside a card.
+   * @param quickReplyButtons Horizontal list of buttons displayed after the content.
+   */
+  constructor(title?: string, text?: string, media?: IMedia, buttons?: Buttons, quickReplyButtons?: Buttons) {
+    super('card');
+    this.title = title;
+    this.text = text;
+    this.media = media;
+    this.buttons = buttons;
+    this.quickReplyButtons = quickReplyButtons;
+  }
 }
--- src/lib/contents/replyable-text.ts
+++ src/lib/contents/replyable-text.ts
@@ -1,23 +1,23 @@
-import { Buttons, IReplyableText } from "../../types";
-import { AbstractContent } from "./abstract-content";
+import { Buttons, IReplyableText } from '../../types';
+import { AbstractContent } from './abstract-content';
 
 /**
  * Implementation of Replyable Text content.
  */
 export class ReplyableTextContent extends AbstractContent implements IReplyableText {
-    text: string;
-    quickReplyButtons?: Buttons;
+  text: string;
+  quickReplyButtons?: Buttons;
 
-    /**
-     * Returns a new `ReplyableTextContent` that can be used to send text and reply buttons to your customer.
-     * 
-     * @param text Text to be sent.
-     * @param quickReployButtons Horizontal list of buttons displayed after the content.
-     */
-    constructor(text: string, quickReplyButtons?: Buttons) {
-        super('replyable_text');
-        this.text = text;
-        this.quickReplyButtons = quickReplyButtons;
-    }
+  /**
+   * Returns a new `ReplyableTextContent` that can be used to send text and reply buttons to your customer.
+   *
+   * @param text Text to be sent.
+   * @param quickReployButtons Horizontal list of buttons displayed after the content.
+   */
+  constructor(text: string, quickReplyButtons?: Buttons) {
+    super('replyable_text');
+    this.text = text;
+    this.quickReplyButtons = quickReplyButtons;
+  }
 
 }
