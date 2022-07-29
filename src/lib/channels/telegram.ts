45fad1c8dcd5daba4cafdeb3041e4abb8e833862
isabela.nascimento <isabela.nascimento@zenvia.com>
2022-07-29 08:34:44 -0300

    Adding quickReply Text e card type to telegram

--- src/lib/channels/telegram.ts
+++ src/lib/channels/telegram.ts
@@ -16,7 +16,7 @@ export class TelegramChannel extends AbstractChannel {
    */
   constructor(token: string, loggerInstance: ILoggerInstance, options: IClientOptions) {
     super(token, 'telegram', loggerInstance, options);
-    this.supportedContents = ['text', 'file'];
+    this.supportedContents = ['text', 'file', 'card', 'replyable_text'];
   }
 
   protected contentSupportValidation(content: IContent): void | never {
